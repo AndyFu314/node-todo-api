61187e9751e2e6c3f6639c86132d45a75934570c
Andy@UDN_mbp <f255431@gmail.com>
2018-02-13 17:00:34 +0800

    Connecting to Mongo and Writing Data
    連線方式參考MongoDB 官方 [3.0 API document] (http://mongodb.github.io/node-mongodb-native/3.0/api/)

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'TodoApp';
+
+MongoClient.connect(url, (err, client) => {
+    if (err) {
+        console.log('Unable to connect to mongo MongoDB server.', err);
+        return 0;
+    }
+    console.log('Connect to MongoDB server.');
+    
+    // client.db(dbName).collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    client.db(dbName).collection('Users').insertOne({
+        name: 'Andy',
+        age: 27,
+        location: 'Taipei'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    })
+
+    client.close();
+});
