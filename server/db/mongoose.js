7d4999448fa9320df7a09291d1b3e788964a43ff
Andy@UDN_mbp <f255431@gmail.com>
2018-02-22 17:05:07 +0800

    Deploy API to Heroku
    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp123');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp123');
 
 module.exports = {
     mongoose,
